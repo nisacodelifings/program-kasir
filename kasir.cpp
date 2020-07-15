df0b02f640822ff3679d77fb52d75b03b6776e52
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-07-15 19:55:36 +0700

    Add files via upload

---
+++ kasir.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+  int diskon,i,j, x, total;
+  int bayar[10], hrga[10],jbrg[10], totali=0;
+  char nama[30][x];
+  
+  cout<<"==============================="<<endl;
+  cout<<"======== Program Kasir ========"<<endl;
+  cout<<"==============================="<<endl;
+  cout<<endl;
+  cout<< "Masukkan Jumlah Data = ";
+  cin>>x;
+  cout << endl;
+
+ for(i=0;i<x;i++){
+  cout<<"==============================="<<endl;
+  cout<<endl;
+  cout<< "Masukkan Nama Barang = ";
+  cin>>nama[i];
+  cout<< "Masukkan Harga Barang = Rp.";
+  cin>>hrga[i];
+  cout<< "Masukkan Jumlah Barang = ";
+  cin>>jbrg[i];
+  cout<<endl;
+ }
+
+system("cls");
+for(i=0;i<x;i++){
+ cout<<"Nama Barang = "<<nama[i]<<endl;
+ cout<<"Harga Barang = Rp. "<<hrga[i]<<endl;
+ cout<<"Jumlah Barang = "<<jbrg[i]<<endl;
+ bayar[i]=hrga[i]*jbrg[i];
+ cout<<"jumlah Belanja = "<<bayar[i]<<endl;
+ cout<<endl;
+ }
+
+for(i=0; i<x; i++){
+ totali=totali+bayar[i];
+ cout<<"Sub total = "<<totali<<endl;
+ } 
+if (totali>30000){
+ diskon=0.1*totali;
+ cout<<"Diskon 10% = Rp. "<<diskon<<endl;
+ total=totali-diskon;
+ cout<<"Total  = Rp. "<<total<<endl;
+ }
+ 
+ return 0;
+}
